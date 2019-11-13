71b8d8f43221dd08432bb63c5110b29081294a08
Dara Heng <dara.heng@bkvfoundry.com>
2019-11-14 09:08:52 +1000

    Refactor: putting clock, form, and select into use

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Clock from './components/clock';
+import FormTest from './components/formTest';
+import SelectTest from './components/selectTest';
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number, index) =>
+<li key={number.toString()}>{number} {index}</li>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Clock /> <br/>
+      <ul>{listItems}</ul>
+      <FormTest />
+      <SelectTest />
     </div>
   );
 }
