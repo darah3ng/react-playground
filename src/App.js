44583ff006d7e0fd00ff682ab350830cbef2d681
Dara Heng <dara.heng@bkvfoundry.com>
2020-01-29 21:49:06 +1000

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import SelectTest from './components/selectTest';
 import MultipleInputs from './components/multipleInputs';
 import Calculator from './components/lifting-state-up/calulator';
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number, index) =>
-<li key={number.toString()}>{number} {index}</li>
-);
-
 function App() {
   return (
     <div>
       <Clock /> <br/>
-      <ul>{listItems}</ul>
       <FormTest />
       <SelectTest />
       <MultipleInputs />
